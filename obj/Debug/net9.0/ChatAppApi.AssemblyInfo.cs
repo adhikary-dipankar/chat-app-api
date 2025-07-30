bc757ca63719c8aaf1e0072f55275db81a1b95e1
Your Name <you@example.com>
2025-07-30 20:49:09 +0530

    docker file updated

--- obj/Debug/net9.0/ChatAppApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatAppApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c417ad02f2798d866852c3f092dd9eed50cccf1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a34b65219dd6724d5322f53b2d0b96dd79f4d4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
